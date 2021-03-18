902614fa7dca692e6be43e59fe1f69dda7ee3d8c
nmu2312 <nmu2312@gmail.com>
2021-03-19 03:29:59 +0900

    add genre search func

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable no-console */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable no-shadow */
-import { FC, useEffect, useState } from 'react';
+import { FC, useEffect, useState, useCallback } from 'react';
 import ShopList from 'components/ShopList';
 import PageNavigation from 'components/PageNavigation';
 import './App.css';
 
 const App: FC = () => {
+  const [isLoading, setIsLoading] = useState(true);
   const [shops, setShops] = useState<unknown[]>([]);
   const [shopsToDisplay, setShopsToDisplay] = useState<unknown[]>([]);
   const [page, setPage] = useState(0);
+  const shopsPerPage = 12;
 
-  const [genre, setGenre] = useState<number[]>([]);
-  const [cashless, setCashless] = useState(false);
+  interface genreObj {
+    [key: string]: string;
+  }
+  const genres = {
+    '2': '和食',
+    '3': '洋食',
+    '4': 'カフェ',
+    '5': 'その他',
+    '6': '中華料理',
+  } as genreObj;
+
+  const [activeGenres, setActiveGenres] = useState(['2', '3', '4', '5', '6']);
   const [area, setArea] = useState(0);
+  const [cashless, setCashless] = useState(false);
   const [takeOut, setTakeOut] = useState(false);
   const [delivery, setDelivery] = useState(false);
 
-  const shopsPerPage = 12;
   useEffect(() => {
     void (async () => {
       // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
@@ -29,64 +43,86 @@ const App: FC = () => {
       setShops(shopData);
       // eslint-disable-next-line no-console
       console.log(shopData);
+      setIsLoading(false);
     })();
   }, []);
 
   useEffect(() => {
-    if (shops.length === 0) {
-      return;
-    }
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const updatedShopsToDisplay = shops.filter((shop: any) => {
-      if (cashless && shop?.custom_fields?.cashless?.[0] !== '1') {
-        return false;
-      }
-      if (takeOut && shop?.custom_fields?.takeout?.[0] !== '1') {
-        return false;
-      }
-      if (delivery && shop?.custom_fields?.delivery?.[0] !== '1') {
-        return false;
-      }
-
-      return true;
-    });
+    const updatedShopsToDisplay = shops.filter(
+      (shop: any) =>
+        !(
+          (cashless && shop?.custom_fields?.cashless?.[0] !== '1') ||
+          (takeOut && shop?.custom_fields?.takeout?.[0] !== '1') ||
+          (delivery && shop?.custom_fields?.delivery?.[0] !== '1') ||
+          (area && shop?.custom_fields?.area) ||
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+          !activeGenres.includes(shop?.genre?.[0]?.toString())
+        ),
+    );
     setShopsToDisplay(updatedShopsToDisplay);
-  }, [shops, genre, cashless, area, takeOut, delivery]);
+    setPage(0);
+  }, [shops, activeGenres, cashless, area, takeOut, delivery]);
 
-  const switchPage = (pageIndex: number): void => {
-    setPage(pageIndex);
-  };
+  const switchPage = useCallback(
+    (pageIndex: number): void => {
+      setPage(pageIndex);
+    },
+    [setPage],
+  );
+
+  const handleGenreChange = useCallback(
+    (genreKey: string) => {
+      if (activeGenres.includes(genreKey)) {
+        setActiveGenres(activeGenres.filter((val) => val !== genreKey));
+      } else {
+        setActiveGenres([...activeGenres, genreKey]);
+      }
+    },
+    [activeGenres],
+  );
 
   return (
     <>
+      ジャンル
+      {Object.keys(genres).map((genreKey) => (
+        <div key={genreKey}>
+          <input
+            type="checkbox"
+            checked={activeGenres.includes(genreKey)}
+            onChange={() => handleGenreChange(genreKey)}
+          />
+          {genres[genreKey]}
+        </div>
+      ))}
       <div>
-        キャッシュレス
         <input
           type="checkbox"
           onChange={() => setCashless(!cashless)}
           checked={cashless}
         />
-        テイクアウト
+        キャッシュレス
         <input
           type="checkbox"
           onChange={() => setTakeOut(!takeOut)}
           checked={takeOut}
         />
-        デリバリー
+        テイクアウト
         <input
           type="checkbox"
           onChange={() => setDelivery(!delivery)}
           checked={delivery}
         />
+        デリバリー
       </div>
-      {shopsToDisplay.length === 0 ? (
-        <div>Loading...</div>
+      {isLoading ? (
+        <>Loading...</>
       ) : (
         <>
-          {shopsToDisplay.length}
+          {shopsToDisplay.length}件
           <PageNavigation
             shops={shopsToDisplay}
             shopsPerPage={shopsPerPage}
+            page={page}
             switchPage={switchPage}
           />
           <ShopList
--- src/components/PageNavigation.tsx
+++ src/components/PageNavigation.tsx
@@ -3,17 +3,32 @@ import { FC } from 'react';
 type Props = {
   shops: unknown[];
   shopsPerPage: number;
+  page: number;
   switchPage: (pageIndex: number) => void;
 };
 
-const PageNavigation: FC<Props> = ({ shops, shopsPerPage, switchPage }) => (
-  <div>
-    {[...Array(Math.ceil(shops.length / shopsPerPage)).keys()].map((n) => (
-      <button type="button" onClick={() => switchPage(n)} key={n}>
-        {n + 1}
-      </button>
-    ))}
-  </div>
+const PageNavigation: FC<Props> = ({
+  shops,
+  shopsPerPage,
+  page,
+  switchPage,
+}) => (
+  <>
+    {shops.length > shopsPerPage && (
+      <div>
+        {[...Array(Math.ceil(shops.length / shopsPerPage)).keys()].map((n) => (
+          <button
+            className={n === page ? 'selected' : ''}
+            type="button"
+            onClick={() => switchPage(n)}
+            key={n}
+          >
+            {n + 1}
+          </button>
+        ))}
+      </div>
+    )}
+  </>
 );
 
 export default PageNavigation;
